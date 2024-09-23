740a7a7db9986d9a673d83c9441006f9d022d598
Tristán-23 <tristan.nouwens@student.gildeopleidingen.nl>
2024-09-23 10:02:19 +0200

    Update for release V2.4.0

--- public/js/cl_index.js
+++ public/js/cl_index.js
@@ -1,2 +1,24 @@
-const baseUrl = "http://127.0.0.1:5000";
-const advancedUrl = baseUrl + "/";
+// Fetches everything inside users
+const server = "fetch";
+const method = server === "update" ? "PATCH" : "POST";
+const table = "users";
+const data = {};
+const change = {};
+
+makeServerRequest(server, method, table, data, change).then((data) => {
+  if (data.type === "ERROR") {
+    test = confirm(
+      server + " failder, " + data.msg + "! Want to see our documentation?"
+    );
+    if (test) {
+      window.location.href = baseUrl + "/explain";
+    }
+    return;
+  } else {
+    console.log(data);
+    test = confirm(server + " was a succes! Want to see our documentation?");
+    if (test) {
+      window.location.href = baseUrl + "/explain";
+    }
+  }
+});
